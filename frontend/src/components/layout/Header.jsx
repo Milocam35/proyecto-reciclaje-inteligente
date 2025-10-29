35208af688c416cbd41652cef495890135312571
camilo <camimillan2003@gmail.com>
2025-10-29 12:21:30 -0500

    Agregando header v1

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,8 +1,24 @@
 
+import { LiaRecycleSolid } from "react-icons/lia";
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="w-full flex items-center justify-between bg-[#3fd3b3] px-8 py-8">
+      {/* Título y breadcrumb */}
+      
+      <div className="flex items-center gap-4">
+        <LiaRecycleSolid size={70} color="white"/>
+        <h1 className="text-3xl font-bold text-white">Ecovision</h1>
+      </div>
+
+      <h1 className="text-3xl font-bold text-white">DashBoard</h1>
+
+      {/* Botón principal */}
+      <button className="bg-white text-gray-800 px-4 py-3 rounded-lg shadow-sm font-medium hover:bg-gray-100 cursor-pointer hover:text-black transition">
+        Create Report
+      </button>
+    </header>
+  );
 }
 
 export default Header
